8aa9ff2f29a235bbc81aebf52ce8a162a58c64d4
Sharvan <sharvangupta79@gmail.com>
2025-11-15 22:57:28 +0530

    committed code

---
+++ FinanceApp/Data/Serivce/IExpensesService.cs
@@ -0,0 +1,12 @@
+﻿using FinanceApp.Models;
+using System.Collections;
+
+namespace FinanceApp.Data.Serivce
+{
+    public interface IExpensesService
+    {
+        Task<IEnumerable<Expense>> GetAll();
+        Task Add(Expense expense);
+        IQueryable GetChartData();
+    }
+}
